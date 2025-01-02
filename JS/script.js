518607c140bacc593d765cd3ad4337996d41ef8b
RomanSleaping <120941516+RomanSleaping@users.noreply.github.com>
2025-01-02 18:35:22 +0200

    777
    777

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+// 1
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+// 2
+const email = "any@gmail.com";
+const checkEmail = email.includes("@");
+const countSymbolsOfEmail = email.indexOf("@");
+console.log (checkEmail, countSymbolsOfEmail, emailLength);
+const emailLength = email.length;
+console.log(emailLength);
+// 3
+const my = "My";
+const name = "name";
+const is = "is";
+const dot = ".";
+let all= `${my} ${name} ${is}`;
+all = all + userName;
+const all2 = my + name + is + userName;
+const userName = "Tolik";
+// 4
+const yourName = "May";
+const payment = 700;
+alert(`Дякую, ${yourname}! До спати ${payment} гривень`)
+// 5
+const fruit = "апельсін";
+console.log(fruit[0]);
+console.log(fruit[fruit.length-1]);
+// 6
+const str = "Jason Statham".toLowerCase();
+const searchStr = "stathaM";
+console.log(str.indexOf(searchStr.toLowerCase()));
+console.log(str.toLowerCase());
+
