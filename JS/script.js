62add5f7e9a22eee84ec3f8806bae0711f7181e0
RomanSleaping <120941516+RomanSleaping@users.noreply.github.com>
2025-01-02 18:46:53 +0200

    777
    777

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,31 @@
+"use strict"
 // 1
-const result = 5 + 5 + "5";
+const result = 5 + 5 + '5';
 console.log(result);
 console.log(typeof result);
 // 2
 const email = "any@gmail.com";
 const checkEmail = email.includes("@");
 const countSymbolsOfEmail = email.indexOf("@");
-console.log (checkEmail, countSymbolsOfEmail, emailLength);
 const emailLength = email.length;
+console.log (checkEmail, countSymbolsOfEmail, emailLength);
+
 console.log(emailLength);
 // 3
 const my = "My";
 const name = "name";
 const is = "is";
 const dot = ".";
+const userName = "Tolik";
 let all= `${my} ${name} ${is}`;
 all = all + userName;
 const all2 = my + name + is + userName;
-const userName = "Tolik";
+
 // 4
 const yourName = "May";
 const payment = 700;
-alert(`Дякую, ${yourname}! До спати ${payment} гривень`)
+const interpulation = `Дякую, ${yourName}! До сплати ${payment} гривень`;
+alert(interpulation);
 // 5
 const fruit = "апельсін";
 console.log(fruit[0]);
